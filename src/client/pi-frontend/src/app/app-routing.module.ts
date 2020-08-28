63d4f5f083c3a40e120269ddbb8e0cc87e0202c5
Hockey <>
2020-08-28 19:30:19 +0700

    handle routing

--- src/client/pi-frontend/src/app/app-routing.module.ts
+++ src/client/pi-frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { from } from 'rxjs';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
