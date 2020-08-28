15b243fe3c7edf0728d9129bb51961cfe365a2fb
Hockey <>
2020-08-28 19:08:30 +0700

    add register component

--- src/client/pi-frontend/src/app/app-routing.module.ts
+++ src/client/pi-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component'
+import { from } from 'rxjs';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/pi-frontend/src/app/app.module.ts
+++ src/client/pi-frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
