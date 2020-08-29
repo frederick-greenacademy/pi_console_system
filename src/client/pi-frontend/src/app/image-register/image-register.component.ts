36b1bf9d0a900df11f25a23091bbc1af7811acc1
Hockey <>
2020-08-29 13:19:43 +0700

    turn of camera when leaving

--- src/client/pi-frontend/src/app/image-register/image-register.component.ts
+++ src/client/pi-frontend/src/app/image-register/image-register.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild, HostListener } from '@angular/core';
-var localStream;
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, HostListener, OnDestroy } from '@angular/core';
+
 @Component({
   selector: 'app-image-register',
   templateUrl: './image-register.component.html',
   styleUrls: ['./image-register.component.css']
 })
-export class ImageRegisterComponent implements OnInit {
+export class ImageRegisterComponent implements OnInit, OnDestroy {
   @ViewChild('video', { static: true }) videoElement: ElementRef;
   @ViewChild('canvas', { static: true }) canvas: ElementRef;
 
@@ -13,42 +13,44 @@ export class ImageRegisterComponent implements OnInit {
   videoHeight = 0;
 
   constraints = {
+    // video: {
+    //   facingMode: "environment",
+    //   width: { ideal: 4096 },
+    //   height: { ideal: 2160 }
+    // }
+    audio: false,
     video: {
-      facingMode: "environment",
-      width: { ideal: 4096 },
-      height: { ideal: 2160 }
+      facingMode: "user",
+      width: { min: 1024, ideal: 1280, max: 1920 },
+      height: { min: 576, ideal: 720, max: 1080 }
     }
   };
+
+  video: any;
+  mediaStream: MediaStream = null;
 
   constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit(): void {
+    this.video = this.videoElement.nativeElement;
     this.startCamera();
   }
-  MediaStream: any;
+
   @HostListener('window:popstate', ['$event'])
   onPopState(event) {
     console.log('Back button pressed', event);
-    // this.renderer.destroy()
-    // localStream.getVideoTracks()[0].stop();
-    // localStream = null;
-    const tracks = localStream.getTracks();
-
-    // Tracks are returned as an array, so if you know you only have one, you can stop it with: 
-    tracks[0].stop();
-
-    // Or stop all like so:
-    tracks.forEach(track => track.stop())
-    // this.renderer.setProperty
-    // this.renderer.removeChild(this.el.nativeElement, this.videoElement.nativeElement);
-
   }
 
   startCamera() {
+    this.stopMediaTracks()
+
     if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
-      navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
+      navigator.mediaDevices.getUserMedia(this.constraints)
+        .then(
+          this.attachVideo.bind(this))
+        .catch(this.handleError);
     } else {
-      alert('Sorry, camera not available.');
+      alert('Rất tiếc, camera không tìm thấy.');
     }
   }
 
@@ -56,19 +58,32 @@ export class ImageRegisterComponent implements OnInit {
     console.log('Error: ', error);
   }
 
-  attachVideo(stream) {
-    // this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+  attachVideo(stream: any) {
+    this.mediaStream = stream;
+    console.log('Stream info: ', stream)
     this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
     this.renderer.listen(this.videoElement.nativeElement, 'play', (event) => {
       this.videoHeight = this.videoElement.nativeElement.videoHeight;
       this.videoWidth = this.videoElement.nativeElement.videoWidth;
     });
-    localStream = stream; // create the stream tracker
   }
 
   capture() {
     this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
     this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
     this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+  }
+
+  private stopMediaTracks() {
+
+    if (this.mediaStream && this.mediaStream.getTracks) {
+      // getTracks() returns all media tracks (video+audio)
+      this.mediaStream.getTracks()
+        .forEach((track: MediaStreamTrack) => track.stop());
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.stopMediaTracks();
   }
 }
