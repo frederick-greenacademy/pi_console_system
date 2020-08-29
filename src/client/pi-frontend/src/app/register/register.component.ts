49893491cbcbc2030d063e81942ee619e3732f8e
Hockey <>
2020-08-29 10:33:31 +0700

    navigate go back

--- src/client/pi-frontend/src/app/register/register.component.ts
+++ src/client/pi-frontend/src/app/register/register.component.ts
@@ -36,6 +36,7 @@ export class RegisterComponent implements OnInit {
   onPopState(event) {
     console.log('Back button pressed', event);
     this.isFinishedForm = false;
+    this.router.navigate(['../'])
   }
 
   ngOnInit() {
