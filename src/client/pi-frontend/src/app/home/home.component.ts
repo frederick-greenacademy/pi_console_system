8e616dfc3e5950c06535533f74806e934802f592
Hockey <>
2020-09-10 22:33:46 +0700

    show welcome message

--- src/client/pi-frontend/src/app/home/home.component.ts
+++ src/client/pi-frontend/src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiHandlerService } from '../services/api-handler.service';
+import { Account } from '../models/account'
+import { from } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // khai bao cac bien
+  user: Account
 
-  constructor() { }
+  constructor(private apiHandler: ApiHandlerService) {
+    this.apiHandler.currentUser.subscribe(u => {
+      this.user = u
+    })
+  }
 
   ngOnInit(): void {
   }
+
 
 }
