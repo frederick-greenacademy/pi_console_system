f2cbb3e114520820b612c9f50f2a7840cfa23409
Hockey <>
2020-09-12 15:16:45 +0700

    add button hover and selected

--- src/client/pi-frontend/src/app/image-register/image-register.component.ts
+++ src/client/pi-frontend/src/app/image-register/image-register.component.ts
@@ -9,14 +9,21 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild, HostListener, OnDe
 export class ImageRegisterComponent implements OnInit, OnDestroy {
   @ViewChild('video', { static: true }) videoElement: ElementRef;
   @ViewChild('canvas', { static: true }) canvas: ElementRef;
+  @ViewChild('canvas2', { static: true }) canvas2: ElementRef;
+  @ViewChild('canvas3', { static: true }) canvas3: ElementRef;
+  @ViewChild('canvas4', { static: true }) canvas4: ElementRef;
+  @ViewChild('canvas5', { static: true }) canvas5: ElementRef;
+  @ViewChild('canvas6', { static: true }) canvas6: ElementRef;
 
   isMobileBrowser = true;
   videoWidth = 0;
   videoHeight = 0;
   imgURL: any
+  imageDatas: any[] = []
+  nextNumberImage: number = 0
+
 
   constraints = {
-
     audio: false,
     video: {
       facingMode: "environment",
@@ -100,17 +107,93 @@ export class ImageRegisterComponent implements OnInit, OnDestroy {
   }
 
   capture() {
-    this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
-    this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
-    // this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
-    const context2d = this.canvas.nativeElement.getContext('2d');
-    context2d.drawImage(this.videoElement.nativeElement, 0, 0);
+    switch (this.nextNumberImage) {
+      case 0:
+        this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
 
-    let imageData: ImageData = null;
-    imageData = context2d.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    console.log('Image data: ', imageData)
+        this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+        var imageData: ImageData = null;
+        imageData = this.canvas.nativeElement.getContext('2d').getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+        if (imageData) {
+          this.nextNumberImage = 1;
+          this.imageDatas.push(imageData)
+        }
+
+        break
+      case 1:
+        this.renderer.setProperty(this.canvas2.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas2.nativeElement, 'height', this.videoHeight);
+
+        this.canvas2.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+        var imageData: ImageData = null;
+        imageData = this.canvas2.nativeElement.getContext('2d').getImageData(0, 0, this.canvas2.nativeElement.width, this.canvas2.nativeElement.height);
+
+        if (imageData) {
+          this.nextNumberImage = 2;
+          this.imageDatas.push(imageData)
+        }
+        break
+      case 2:
+        this.renderer.setProperty(this.canvas3.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas3.nativeElement, 'height', this.videoHeight);
+
+        this.canvas3.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+        var imageData: ImageData = null;
+        imageData = this.canvas3.nativeElement.getContext('2d').getImageData(0, 0, this.canvas3.nativeElement.width, this.canvas3.nativeElement.height);
+
+        if (imageData) {
+          this.nextNumberImage = 3;
+          this.imageDatas.push(imageData)
+        }
+        break
+      case 3:
+        this.renderer.setProperty(this.canvas4.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas4.nativeElement, 'height', this.videoHeight);
+
+        this.canvas4.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+        var imageData: ImageData = null;
+        imageData = this.canvas4.nativeElement.getContext('2d').getImageData(0, 0, this.canvas4.nativeElement.width, this.canvas4.nativeElement.height);
+
+        if (imageData) {
+          this.nextNumberImage = 4;
+          this.imageDatas.push(imageData)
+        }
+        break
+      case 4:
+        this.renderer.setProperty(this.canvas5.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas5.nativeElement, 'height', this.videoHeight);
+
+        this.canvas5.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+        var imageData: ImageData = null;
+        imageData = this.canvas5.nativeElement.getContext('2d').getImageData(0, 0, this.canvas5.nativeElement.width, this.canvas5.nativeElement.height);
+
+        if (imageData) {
+          this.nextNumberImage = 5;
+          this.imageDatas.push(imageData)
+        }
+        break
+      case 5:
+        this.renderer.setProperty(this.canvas6.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas6.nativeElement, 'height', this.videoHeight);
+
+        this.canvas6.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
+        var imageData: ImageData = null;
+        imageData = this.canvas6.nativeElement.getContext('2d').getImageData(0, 0, this.canvas6.nativeElement.width, this.canvas6.nativeElement.height);
+
+        if (imageData) {
+          this.nextNumberImage = 1;
+          this.imageDatas.push(imageData)
+        }
+        break
+    }
+
   }
 
+  setNextNumberImage(e: number) {
+    this.nextNumberImage = e
+  }
   private stopMediaTracks() {
 
     if (this.mediaStream && this.mediaStream.getTracks) {
